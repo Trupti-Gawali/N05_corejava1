d5cea840d61c534f7554a7edb50748985ab616d3
Trupti <Trupti@LAPTOP-J3R8QI9B>
2023-07-21 11:26:17 +0530

    Second commit

---
+++ dayone/src/org/tnsif/introduction/IncrementOperatorDemo.java
@@ -0,0 +1,15 @@
+//program to demonstrate pre and post increment
+package org.tnsif.introduction;
+public class IncrementOperatorDemo {
+
+	public static void main(String[] args) {
+		int p=11,q=2;
+		int x=p++;
+		int y=p+q;
+		++q;
+		System.out.println(p);
+		System.out.println(q);
+
+	}
+
+}
